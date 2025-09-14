314ccb1293636ce26205600b462a7a4eddde345d
Joshua Zhang <53535920+JoshuaLeeZhang@users.noreply.github.com>
2025-09-14 01:45:55 -0400

    Format wallet balances consistently

--- src/NGO-dashboard/lib/utils.ts
+++ src/NGO-dashboard/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(amount: number) {
+  return amount.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })
+}
